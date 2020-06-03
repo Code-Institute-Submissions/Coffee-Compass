e4afa2c927876bceb941d5bf163314057b55ffb5
xander-webdev <xwernecke@gmail.com>
2020-06-03 15:19:58 +0000

    added the comparechoice function to compare the player and computer choice and output the result to the console

--- js/rockpaperscissors.js
+++ js/rockpaperscissors.js
@@ -10,37 +10,60 @@ const game = () => {
     const computerOptions = ["spock", "paper", "rock", "lizard", "scissors"];
     let playerChoice = " ";
 
-    options.forEach((option)=>{
-        option.addEventListener("click", function(){
-            computerNumber = Math.floor(Math.random()*5);
-            computerChoice = computerOptions[computerNumber];
-            playerChoice = option.id;
-            compareChoice(playerChoice,computerChoice);
-        });
+    options.forEach((option) => {
+      option.addEventListener("click", function () {
+        let computerNumber = Math.floor(Math.random() * 5);
+        let computerChoice = computerOptions[computerNumber];
+        let playerChoice = option.id;
+        compareChoice(playerChoice, computerChoice);
+      });
     });
   };
   //use a function to compare player and computer choice
- const computerChoice = (playerChoice,computerChoice) =>{
-     const result = document.querySelector(".result-text");
-//checking for a tie
-if (playerChoice === computerChoice){
-    console.log("It is a tie!");
+  const compareChoice = (playerChoice, computerChoice) => {
+    const result = document.querySelector(".result-text");
+    //checking for a tie
+    if (playerChoice === computerChoice) {
+      console.log("It is a tie!");
+      //check for spock
+    } else if (playerChoice === "spock") {
+      if (computerChoice === "rock" || computerChoice === "scissors") {
+        console.log(`player wins: ${playerChoice} computer loses:${computerChoice}`)
+      } else {
+        console.log(`player loses: ${playerChoice} computer wins:${computerChoice}`)
+      }
+      //check for paper
+    } else if (playerChoice === "paper") {
+      if (computerChoice === "rock" || computerChoice === "spock") {
+        console.log(`player wins: ${playerChoice} computer loses:${computerChoice}`)
+      } else {
+        console.log(`player loses: ${playerChoice} computer wins:${computerChoice}`)
+      }
+      //check for rock
+    } else if (playerChoice === "rock") {
+      if (computerChoice === "scissors" || computerChoice === "lizard") {
+        console.log(`player wins: ${playerChoice} computer loses:${computerChoice}`)
+      } else {
+        console.log(`player loses: ${playerChoice} computer wins:${computerChoice}`)
+      }
+      //check for lizard
+    } else if (playerChoice === "lizard") {
+      if (computerChoice === "spock" || computerChoice === "paper") {
+        console.log(`player wins: ${playerChoice} computer loses:${computerChoice}`)
+      } else {
+        console.log(`player loses: ${playerChoice} computer wins:${computerChoice}`)
+      }
+      //check for scissors
+    } else if (playerChoice === "scissors") {
+      if (computerChoice === "paper" || computerChoice === "lizard") {
+        console.log(`player wins: ${playerChoice} computer loses:${computerChoice}`)
+      } else {
+        console.log(`player loses: ${playerChoice} computer wins:${computerChoice}`)
+      }
+    }
+  }
 
-}
-//check for spock
-else if (playerChoice === "spock")
-//check for paper
-
-//check for rock
-
-//check for lizard
-
-//check for scissors
-
-
-
- }
-playGame();
+  playGame();
 };
 
 game();
