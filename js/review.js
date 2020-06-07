c291ea84d14a3f8b787221ef3213bd3b22acadd0
xander-webdev <xwernecke@gmail.com>
2020-06-07 18:30:54 +0000

    I added js functionality to the form so that reviews can be added and stored on the dom. also added a button so that users can navigate back to the maps to continue to search for coffee

--- js/review.js
+++ js/review.js
@@ -1,19 +1,48 @@
-const Review = () => {
-    let name = document.querySelector('#name');
-    let order = document.querySelector("#order");
-    let rating = document.querySelector("#rating");
-    let tContent = document.querySelector(".table-content");
-    //referenced code from stack overflow
-    document.querySelector('form#input-form').addEventListener('submit', function (e) {
+//I referenced a lot of this code from a project I did on Travery Media's Udemy Course Javascript From The Beginning booklist project which I adapted for use of pushing the reviews to the dom as this was out of my scope of knowledge. I did however write this javascript over and over again until I could write it without refering to the code from the project at all. The syntax may be similar as that project.
 
-        //prevent the normal submission of the form
+const reviewFunc = () => {
+    //review constructor
+    function Review(name, order, rating) {
+        this.name = name;
+        this.order = order;
+        this.rating = rating;
+    }
+    //UI constructors
+    function UI() {}
+
+    //add review to dom
+    ////create the tr element, insert cols and append
+    ////clear fields
+    UI.prototype.pushToDOM = function (review) {
+        let tContent = document.querySelector("#table-content");
+        let reviewRow = document.createElement("tr");
+        reviewRow.innerHTML =
+            `
+        <td>${review.name}</td>
+        <td>${review.order}</td>
+        <td>${review.rating}</td>
+        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+        `;
+        tContent.appendChild(reviewRow);
+        let name = document.querySelector("#name").value = "";
+        let order = document.querySelector("#order").value = "";
+        let rating = document.querySelector("#rating").value = "";
+    }
+
+    //event listener
+    document.querySelector("#input-form").addEventListener("submit", (e) => {
+        let name = document.querySelector("#name").value;
+        let order = document.querySelector("#order").value;
+        let rating = document.querySelector("#rating").value;
+
+        let review = new Review(name, order, rating)
+        let ui = new UI();
+        ui.pushToDOM(review);
         e.preventDefault();
-     
-
-
     });
-
-
+    //delete review 
+    //save to the browswer
 
 }
-Review()
+reviewFunc();
+
