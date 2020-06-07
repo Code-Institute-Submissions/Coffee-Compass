60221f0c55697674c97caa1cd2ccf3fe3e935eb0
xander-webdev <xwernecke@gmail.com>
2020-06-07 10:41:13 +0000

    started over with the JS code as I needed to add the values and push it to the dom as an object not as separate values

--- js/review.js
+++ js/review.js
@@ -3,18 +3,16 @@ const Review = () => {
     let order = document.querySelector("#order");
     let rating = document.querySelector("#rating");
     let tContent = document.querySelector(".table-content");
-        //referenced code from stack overflow
-        document.querySelector('form#input-form').addEventListener('submit', function (e) {
+    //referenced code from stack overflow
+    document.querySelector('form#input-form').addEventListener('submit', function (e) {
 
         //prevent the normal submission of the form
         e.preventDefault();
-        tContent.innerHTML=`<td>${name.value}</td><td>${order.value}</td><td>${rating.value}</td>`
-        
-        
+     
 
-       
+
     });
-    
+
 
 
 }
