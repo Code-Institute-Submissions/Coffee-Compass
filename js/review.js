79f20c57254f8d719fdc277759a6fa68a4fbcb58
xander-webdev <xwernecke@gmail.com>
2020-06-07 07:42:25 +0000

    pushed the values of the input fields to the dom to see the layout

--- js/review.js
+++ js/review.js
@@ -1,19 +1,20 @@
 const Review = () => {
     let name = document.querySelector('#name');
     let order = document.querySelector("#order");
-    let rating = document.querySelector("#rating")
+    let rating = document.querySelector("#rating");
+    let tContent = document.querySelector(".table-content");
         //referenced code from stack overflow
         document.querySelector('form#input-form').addEventListener('submit', function (e) {
 
         //prevent the normal submission of the form
         e.preventDefault();
-
-        console.log(name.value,order.value,rating.value);
+        tContent.innerHTML=`<td>${name.value}</td><td>${order.value}</td><td>${rating.value}</td>`
+        
         
 
        
     });
-    let name = ""
+    
 
 
 }
