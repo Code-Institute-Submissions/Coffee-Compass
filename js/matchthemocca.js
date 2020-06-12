78f6805893546d5affc295b8043c5203ebc06966
xander-webdev <xwernecke@gmail.com>
2020-06-12 13:13:14 +0000

    added the array that contains the images to be displayed on the game

--- js/matchthemocca.js
+++ js/matchthemocca.js
@@ -1,4 +1,55 @@
-const game = ()=>{
+const game = () => {
+//the card array that will display all of the cards
+    const cardArray = [{
+            name: "coffee1",
+            img: "assets/img/coffee1.png"
+        },
+        {
+            name: "coffee1",
+            img: "assets/img/coffee1.png"
+        },
+        {
+            name: "coffee2",
+            img: "assets/img/coffee2.png"
+        },
+        {
+            name: "coffee2",
+            img: "assets/img/coffee2.png"
+        },
+        {
+            name: "coffee3",
+            img: "assets/img/coffee3.png"
+        },
+        {
+            name: "coffee3",
+            img: "assets/img/coffee3.png"
+        },
+        {
+            name: "coffee4",
+            img: "assets/img/coffee4.png"
+        },
+        {
+            name: "coffee4",
+            img: "assets/img/coffee4.png"
+        },
+        {
+            name: "coffee5",
+            img: "assets/img/coffee5.png"
+        },
+        {
+            name: "coffee5",
+            img: "assets/img/coffee5.png"
+        },
+        {
+            name: "coffee6",
+            img: "assets/img/coffee6.png"
+        },
+        {
+            name: "coffee6",
+            img: "assets/img/coffee6.png"
+        }
+    ];
+
 
 }
 game();
