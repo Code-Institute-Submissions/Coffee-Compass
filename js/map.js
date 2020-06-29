26da533e27f11a1832a926940b19108acec78756
xander-webdev <xwernecke@gmail.com>
2020-06-29 14:35:06 +0000

    added the places functionality so that it brings up places with nearby the users location with data including rating, address and the name of the cafe

--- js/map.js
+++ js/map.js
@@ -4,47 +4,87 @@ let usersCurrentLocation;
 let coffeeInfo;
 
 //reload map and refresh users location
-document.querySelector("#btn-locate").addEventListener("click", function(e){
-    initMap();
-    e.preventDefault()
-})
+document.querySelector("#btn-locate").addEventListener("click", function (e) {
+  initMap();
+  e.preventDefault();
+});
 function initMap() {
-    map = new google.maps.Map(document.querySelector('#map'), {
-        center: {
-            lat: -34.397,
-            lng: 150.644
-        },
-        zoom: 10
-    });
-    usersCurrentLocation = new google.maps.InfoWindow;
-    coffeeInfo = new google.maps.infoWindow;
+  map = new google.maps.Map(document.querySelector("#map"), {
+    center: {
+      lat: -34.397,
+      lng: 150.644,
+    },
+    zoom: 10,
+  });
+  usersCurrentLocation = new google.maps.InfoWindow();
+  coffeeInfo = new google.maps.InfoWindow();
 
-    //asks the user for location information and check if it/s been allowed
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            let pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-//you were here before saving
-            usersCurrentLocation.setPosition(pos);
-            usersCurrentLocation.setContent('You are here!');
-            usersCurrentLocation.open(map);
-            map.setCenter(pos);
-        }, function () {
-            handleLocationError(true, usersCurrentLocation, map.getCenter());
-        });
-    } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, usersCurrentLocation, map.getCenter());
-    }
-}
+  //asks the user for location information and check if it/s been allowed
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      function (position) {
+        let pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+        //setting the conditions for the geolocation
+        usersCurrentLocation.setPosition(pos);
+        usersCurrentLocation.setContent("You are here!");
+        usersCurrentLocation.open(map);
+        map.setCenter(pos);
+        //setting the conditions for the automatic search of the cafes nearby
+        let request = {
+          location: pos,
+          radius: 8000,
+          type: ["cafe"]
+        };
+        //calling the google maps places and calling the nearby method
+        service = new google.maps.places.PlacesService(map);
+        service.nearbySearch(request, callback);
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    usersCurrentLocation.setPosition(pos);
-    usersCurrentLocation.setContent(browserHasGeolocation ?
-        'Error: The Geolocation service failed.' :
-        'Error: Your browser doesn\'t support geolocation.');
-    usersCurrentLocation.open(map);
+        function callback(results, status) {
+          if (status == google.maps.places.PlacesServiceStatus.OK) {
+            map.setZoom(12);
+            for (let i = 0; i < results.length; i++) {
+              createMarker(results[i], coffeeInfo);
+            }
+          }
+        }
+      },
+      function () {
+        handleLocationError(true, usersCurrentLocation, map.getCenter());
+      }
+    );
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, usersCurrentLocation, map.getCenter());
+  }
 }
-initMap()
+//creating the markers and adding a drop animation to it
+function createMarker(place, coffeeInfo) {
+  let marker = new google.maps.Marker({
+    map: map,
+    draggable: false,
+    animation: google.maps.Animation.DROP,
+    position: place.geometry.location,
+  });
+
+  google.maps.event.addListener(marker, "click", function () {
+      let cafeName = place.name;
+      let cafeRating = place.rating;
+      let cafeAd = place.formatted_address;
+    coffeeInfo.setContent("<strong>Name: </strong>" + cafeName + "<br>" + "<strong>Rating: </strong>" + cafeRating + "/5" + "<br>"+ "<strong>Address: </strong>" + cafeAd);
+    coffeeInfo.open(map, this);
+  });
+};
+
+function handleLocationError(browserHasGeolocation, usersCurrentLocation, pos) {
+  usersCurrentLocation.setPosition(pos);
+  usersCurrentLocation.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  usersCurrentLocation.open(map);
+};
+initMap();
